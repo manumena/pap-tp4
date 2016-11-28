053692c7545fdaa8e5735a7821005a821868d556
manumena <manuelmena1993@gmail.com>
2016-11-27 21:14:42 -0300

    Add exercise 2 solver

--- src/ej2/ej2.cpp
+++ src/ej2/ej2.cpp
@@ -7,52 +7,91 @@ using namespace std;
 **  Problem solver
 */
 
-void run_solver() {}
+int minCount(vector<int> vec) {
+	int min = vec[0];
+	int count = 1;
+	for(int i = 1; i < vec.size(); i++) {
+		if(vec[i] < min) {
+			min = vec[i];
+			count = 1;
+		}
+		else if(vec[i] == min)
+			count++;
+	}
+	return count;
+}
+
+float expectedValue(vector<int> vec) {
+	float mean = 0.0;
+	mean = (float) vec.size() / minCount(vec);
+	cout << mean << endl;
+
+	for(int i = 1; i < vec.size(); i++) {
+		vector<int> subVec(vec.begin() + i, vec.end());
+		mean += (float) subVec.size() / minCount(subVec) - 1;
+	}
+
+	return mean;
+}
+
+void run_solver() {
+	int n;
+	cin >> n;
+
+	vector<int> vec;
+	for(int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		vec.push_back(num);
+	}
+
+	cout << expectedValue(vec) << endl;
+}
 
 // MAIN
 
 enum mode {solver, test};
 
 int main (int argc, char* argv[]) {
-    // Configuración de la ejecución
-    mode exec_mode = solver;
+	// Configuración de la ejecución
+	mode exec_mode = solver;
 
-    // Parsea las opciones recibidas
-    char opt;
-    while ((opt = getopt(argc, argv, "ht")) != -1) {
-        switch (opt) {
-            case 'h': { // mostrar ayuda
-                show_help(argv[0]);
-                exit(0);
-                break;
-            }
-            case 't': { // correr tests unitarios
-                exec_mode = test;
-                break;
-            }
-            default: { // si las opciones son inválidas
-                show_help(argv[0]);
-                exit(1);
-                break;
-            }
-        }
-    }
+	// Parsea las opciones recibidas
+	char opt;
+	while ((opt = getopt(argc, argv, "ht")) != -1) {
+		switch (opt) {
+			case 'h': { // mostrar ayuda
+				show_help(argv[0]);
+				exit(0);
+				break;
+			}
+			case 't': { // correr tests unitarios
+				exec_mode = test;
+				break;
+			}
+			default: { // si las opciones son inválidas
+				show_help(argv[0]);
+				exit(1);
+				break;
+			}
+		}
+	}
 
-    if (exec_mode == solver) {
-        run_solver();
-    }
-    else if (exec_mode == test) {
-        run_unit_tests();
-    }
+	if (exec_mode == solver) {
+		run_solver();
+	}
+	else if (exec_mode == test) {
+		run_unit_tests();
+	}
 }
 
 // Imprime por pantalla un texto de ayuda
 void show_help(char* bin_path) {
-    cout << "  Modo de uso: " << setw(12) << bin_path << endl;
-    cout << endl;
-    cout << "  Los parámetros se reciben a través de la entrada estándar." << endl;
-    cout << endl;
-    cout << "  Opciones:" << endl;
-    cout << "    -h          Muestra este texto de ayuda" << endl;
-    cout << "    -t          Ejecuta los tests unitarios provistos para el algoritmo" << endl;
+	cout << "  Modo de uso: " << setw(12) << bin_path << endl;
+	cout << endl;
+	cout << "  Los parámetros se reciben a través de la entrada estándar." << endl;
+	cout << endl;
+	cout << "  Opciones:" << endl;
+	cout << "    -h          Muestra este texto de ayuda" << endl;
+	cout << "    -t          Ejecuta los tests unitarios provistos para el algoritmo" << endl;
 }
